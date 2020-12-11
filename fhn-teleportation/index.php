8e895986a7451c3011a2ba2ee1540520d65f25e7
Abouzar Kaboudian (MacBook-Pro) <abouzar.kaboudian@physics.gatech.edu>
2020-12-10 21:12:38 -0500

    new file:   fhn-teleportation/2dPhaseMap.frag 	new file:   fhn-teleportation/Makefile 	new file:   fhn-teleportation/abubu_app.css 	new file:   fhn-teleportation/app.js 	new file:   fhn-teleportation/click.frag 	new file:   fhn-teleportation/defib.frag 	new file:   fhn-teleportation/fhn.html 	new file:   fhn-teleportation/index.html 	new file:   fhn-teleportation/index.php 	new file:   fhn-teleportation/init.frag 	new file:   fhn-teleportation/march.frag 	new file:   fhn-teleportation/project.zsh 	new file:   fhn-teleportation/shader.php 	modified:   index.html

---
+++ fhn-teleportation/index.php
@@ -0,0 +1,70 @@
+<!doctype html>
+<html>
+<head>
+    <title>FHN</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
+    <script src='https://abubujs.org/libs/Abubu.latest.js' 
+	    type='text/javascript'></script>
+
+    <link rel="stylesheet" type="text/css" href="abubu_app.css">
+
+</head>
+
+<!--&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&-->
+<!-- body of the html page                                             -->
+<!--&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&-->
+<body>
+    <h1>The FitzHugh-Nagumo Model</h1>
+    <table>
+        <tr>
+            <td>
+                <canvas id=canvas_1 width=512 height=512>
+                    Your browser doesn't support HTML5.0
+                </canvas>
+            </td>
+            <td>
+                <canvas id=canvas_2 width=512 height=512>
+                    Your browser doesn't support HTML5.0
+                </canvas>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <canvas id=canvas_3 width=512 height=512>
+                    Your browser doesn't support HTML5.0
+                </canvas>
+            </td>
+            <td>
+                <canvas id=canvas_4 width=512 height=512>
+                    Your browser doesn't support HTML5.0
+                </canvas>
+            </td>
+        </tr>
+    </table>
+
+<!--&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&-->
+<!-- All shaders included here (codes written in GLSL)                 -->
+<!--&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&-->
+
+<?php
+    include "shader.php" ;
+    shader('init' ) ;
+    shader('march') ;
+    shader('click') ;
+    shader('2dPhaseMap') ;
+    shader('defib') ;
+?>
+
+<!--&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&-->
+<!-- main script - JavaScript code                                     -->
+<!--&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&-->
+<script>
+<?php
+    echo file_get_contents( __dir__ . "/app.js" ) ;
+?>
+</script>
+
+</body>
+</html>
